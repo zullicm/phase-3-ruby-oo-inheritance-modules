35203f9bd6c4f9d0cefa082f5e466d0aff4b8667
Christopher Marzulli <chriszulli15@gmail.com>
2022-08-17 12:28:57 -0400

    commit tree eedbbb884f44cac3dee4cc59caf84440e9b2a4bf

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,12 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+class Kid
+  
+  extend MetaDancing
+  include Dance
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name 
+  end
+end
